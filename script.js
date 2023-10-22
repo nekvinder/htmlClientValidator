443ca7fba3c2af44cc90093e3be3b9471081e22f
nekvinder <nekvinder@gmail.com>
2023-10-22 13:49:27 +0530

    refactor

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const phoneInput = document.querySelector("#phone");
-const iti = window.intlTelInput(phoneInput, {
+const phoneInputEl = document.querySelector("#phone");
+const nameInputEl = document.getElementById("name");
+const emailInputEl = document.getElementById("email");
+
+const iti = window.intlTelInput(phoneInputEl, {
   initialCountry: "auto",
   utilsScript:
     "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.0/js/utils.js",
 });
+
+//validation login start
 
 function setInvalid(fieldName, fieldErrorName, errorMessage) {
   document.getElementById(fieldErrorName).textContent = errorMessage;
@@ -16,7 +21,7 @@ function validateName() {
   const fieldErrorName = "nameError";
 
   setInvalid(fieldName, fieldErrorName, "");
-  const userName = document.getElementById("name").value;
+  const userName = nameInputEl.value;
   if (userName === "") {
     setInvalid(fieldName, fieldErrorName, "Name is mandatory!");
     return false;
@@ -31,7 +36,7 @@ function validateEmail() {
   const fieldName = "email";
   const fieldErrorName = "emailError";
 
-  const email = document.getElementById("email").value;
+  const email = emailInputEl.value;
   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   setInvalid(fieldName, fieldErrorName, "");
   if (email === "") {
@@ -75,102 +80,20 @@ function buttonValidator() {
   submitButton.style.opacity = isValid ? 1 : 0.5;
 }
 
-const eventName = "blur";
-document.getElementById("name").addEventListener(eventName, function() {
-  validateName();
-  buttonValidator();
-});
+function main() {
+  const eventName = "blur";
+  const inputValidatorMaps = [
+    { input: nameInputEl, validator: validateName },
+    { input: emailInputEl, validator: validateEmail },
+    { input: phoneInputEl, validator: validatePhone },
+  ];
 
-document.getElementById("email").addEventListener(eventName, function() {
-  validateEmail();
-  buttonValidator();
-});
+  for (const entry of inputValidatorMaps) {
+    entry.input.addEventListener(eventName, function() {
+      entry.validator();
+      buttonValidator();
+    });
+  }
+}
 
-phoneInput.addEventListener(eventName, function() {
-  validatePhone();
-  buttonValidator();
-});
-
-// // Import intlTelInput
-// import intlTelInput from 'intl-tel-input';
-
-// // Function to validate the name field
-// function validateName() {
-//   const nameInput = document.getElementById('name');
-//   const nameError = document.getElementById('nameError');
-//   const nameValue = nameInput.value.trim();
-
-//   if (nameValue.length < 3) {
-//     nameInput.style.borderColor = 'red';
-//     nameError.style.color = 'red';
-//     nameError.textContent = 'Name must be at least 3 characters';
-//     return false;
-//   } else {
-//     nameInput.style.borderColor = '';
-//     nameError.textContent = '';
-//     return true;
-//   }
-// }
-
-// // Function to validate the email field using regex
-// function validateEmail() {
-//   const emailInput = document.getElementById('email');
-//   const emailError = document.getElementById('emailError');
-//   const emailValue = emailInput.value.trim();
-//   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-//   if (!emailPattern.test(emailValue)) {
-//     emailInput.style.borderColor = 'red';
-//     emailError.style.color = 'red';
-//     emailError.textContent = 'Invalid email address';
-//     return false;
-//   } else {
-//     emailInput.style.borderColor = '';
-//     emailError.textContent = '';
-//     return true;
-//   }
-// }
-
-// // Function to validate the phone field using intl-tel-input
-// function validatePhone() {
-//   const phoneInput = document.getElementById('phone');
-//   const phoneError = document.getElementById('phoneError');
-//   const phoneValue = phoneInput.value.trim();
-//   const telInput = intlTelInput(phoneInput);
-//   const isValidNumber = telInput.isValidNumber();
-
-//   if (!isValidNumber) {
-//     phoneInput.style.borderColor = 'red';
-//     phoneError.style.color = 'red';
-//     phoneError.textContent = 'Invalid phone number';
-//     return false;
-//   } else {
-//     phoneInput.style.borderColor = '';
-//     phoneError.textContent = '';
-//     return true;
-//   }
-// }
-
-// // Add event listeners for real-time validation
-// const nameInput = document.getElementById('name');
-// const emailInput = document.getElementById('email');
-// const phoneInput = document.getElementById('phone');
-// const form = document.getElementById('form');
-// const submitButton = document.getElementById('submitButton');
-
-// nameInput.addEventListener('input', validateName);
-// emailInput.addEventListener('input', validateEmail);
-// phoneInput.addEventListener('input', validatePhone);
-// form.addEventListener('submit', handleSubmit);
-
-// // Initially disable the submit button
-// submitButton.disabled = true;
-
-// // Enable the submit button when the form is valid
-// form.addEventListener('input', () => {
-//   if (validateName() && validateEmail() && validatePhone()) {
-//     submitButton.disabled = false;
-//   } else {
-//     submitButton.disabled = true;
-//   }
-// });
+main();
